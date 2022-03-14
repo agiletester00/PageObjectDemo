7f5d4943622e932ec6b738d7b308a68db01f46ca
Sandeep Kumar <agiletester00@gmail.com>
2022-03-15 01:28:31 +0530

    New File TestDemo Added

---
+++ src/BasicAllUtils/TestDemo.java
@@ -0,0 +1,29 @@
+package BasicAllUtils;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	
+	
+	@Test
+	public void Testde()
+	{
+		System.out.println("Test1");
+	}
+
+	@Test
+	public void Testde1()
+	{
+		System.out.println("Test2");
+	}
+	
+	@Test
+	public void Testde2()
+	{
+		System.out.println("Test3");
+	}
+	
+	
+	
+	
+}
